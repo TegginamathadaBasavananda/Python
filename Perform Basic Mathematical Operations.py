f2e24c6c6a9c826ba23e5a5af810d77c9154fc5d
TegginamathadaBasavananda <basavanandabasu007@gmail.com>
2025-10-03 21:47:42 +0530

    task 1 and task 2

---
+++ Perform Basic Mathematical Operations.py
@@ -0,0 +1,10 @@
+num1 =float(input("enter first number \n"))
+num2 =float(input(" enter second number \n"))
+addition = num1 + num2
+substraction = (num1 - num2)
+multiplication = (num1 * num2)
+division = (num1 / num2)
+print("addition =",addition)
+print("substraction =",substraction)
+print("multiplication =",multiplication)
+print("division =",division)
